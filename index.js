0da5c75d1b68816e6e1abf04e9d49577b6323521
Henry <cryptix@riseup.net>
2017-11-19 08:50:20 +0100

    fork patch-settings

--- index.js
+++ index.js
@@ -3,63 +3,54 @@ const { Value, computed } = require('mutant')
 const get = require('lodash.get')
 const merge = require('lodash.merge')
 
-const STORAGE_KEY = 'patchSettings'
+const STORAGE_KEY = 'patchDrafts'
 
 const gives = nest({
-  'settings.sync.get': true,
-  'settings.sync.set': true,
-  'settings.obs.get': true,
+  'drafts.sync.get': true,
+  'drafts.sync.set': true,
 })
 
 const create = (api) => {
-  var _settings
+  var _drafts
 
   return nest({
-    'settings.sync.get': getSync,
-    'settings.sync.set': setSync,
-    'settings.obs.get': getObs,
+    'drafts.sync.get': getSync,
+    'drafts.sync.set': setSync,
   })
 
-  function getSync (path, fallback) {
+  function getSync (msgKey) {
     _initialise()
-    if (!path) return _settings()
-
-    return get(_settings(), path, fallback)
+    return get(_drafts(), path)
   }
 
-  function setSync (newSettings) {
+  function setSync (msgKey, text) {
     _initialise()
 
-    const updatedSettings = merge({}, _settings(), newSettings)
-    _settings.set(updatedSettings)
-  }
-
-  function getObs (path, fallback) {
-    _initialise()
-    if (!path) return _settings
-
-    return computed(_settings, s => get(s, path, fallback))
+    const updatedDrafts = merge({}, _drafts(), {
+      [msgKey]: text
+    })
+    _drafts.set(updatedDrafts)
   }
 
   function _initialise () {
-    if (_settings) return
+    if (_drafts) return
 
     const settings = localStorage[STORAGE_KEY]
       ? JSON.parse(localStorage[STORAGE_KEY])
       : {}
-    _settings = Value(settings)
+    _drafts = Value(settings)
 
     // initialise a listener to persist on changes
-    _settings(_save)
+    _drafts(_save)
   }
 
-  function _save (newSettings) {
-    localStorage[STORAGE_KEY]= JSON.stringify(newSettings)
+  function _save (newDrafts) {
+    localStorage[STORAGE_KEY]= JSON.stringify(newDrafts)
   }
 }
 
 module.exports = {
-  patchSettings: {
+  patchDrafts: {
     gives,
     create
   }
